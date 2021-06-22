53e5ac7afa49b45431620d3831cb8e87d0bf6dab
MerleL <me_rle@web.de>
2021-06-22 15:19:35 +0200

    BenachrichtigungenScreen: - Bereiche bildlich voneinander abgrenzen

--- lib/screens/benachrichtigungen_screen.dart
+++ lib/screens/benachrichtigungen_screen.dart
@@ -8,45 +8,171 @@ class BenachrichtigungenScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: SafeArea(
-        child: Container(
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-              children: [
-                buildHeader(context),
-                buildListTile(
-                  context,
-                  'Pop-Up-Benachrichtigungen',
-                  Icon(
-                    Icons.check_box,
-                    color: Color.fromARGB(255, 78, 90, 92),
+        child: SingleChildScrollView(
+          child: Container(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  buildHeader(context),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10.0),
+                    child: Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(200, 240, 240, 240),
+                        border: Border.all(
+                          color: Color.fromARGB(255, 200, 200, 200),
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(4.0),
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10.0),
+                            child: Text('Pop-Up-Benachrichtigungen'),
+                          ),
+                          buildListTile(
+                            context,
+                            'Einzel-Chats',
+                            Icon(
+                              Icons.check_box,
+                              color: Color.fromARGB(255, 78, 90, 92),
+                            ),
+                          ),
+                          buildListTile(
+                            context,
+                            'Gruppen-Chats',
+                            Icon(
+                              Icons.check_box,
+                              color: Color.fromARGB(255, 78, 90, 92),
+                            ),
+                          ),
+                          buildListTile(
+                            context,
+                            'Fachbereiche',
+                            Icon(
+                              Icons.check_box,
+                              color: Color.fromARGB(255, 78, 90, 92),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                ),
-                buildListTile(
-                  context,
-                  'Töne',
-                  Icon(
-                    Icons.check_box,
-                    color: Color.fromARGB(255, 78, 90, 92),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10.0),
+                    child: Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(200, 240, 240, 240),
+                        border: Border.all(
+                          color: Color.fromARGB(255, 200, 200, 200),
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(4.0),
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10.0),
+                            child: Text('Töne'),
+                          ),
+                          buildListTile(
+                            context,
+                            'Pop-Up',
+                            Icon(
+                              Icons.check_box,
+                              color: Color.fromARGB(255, 78, 90, 92),
+                            ),
+                          ),
+                          buildListTile(
+                            context,
+                            'In-App',
+                            Icon(
+                              Icons.check_box,
+                              color: Color.fromARGB(255, 78, 90, 92),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                ),
-                buildListTile(
-                  context,
-                  'Vibration',
-                  Icon(
-                    Icons.check_box,
-                    color: Color.fromARGB(255, 78, 90, 92),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10.0),
+                    child: Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(200, 240, 240, 240),
+                        border: Border.all(
+                          color: Color.fromARGB(255, 200, 200, 200),
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(4.0),
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10.0),
+                            child: Text('Vibration'),
+                          ),
+                          buildListTile(
+                            context,
+                            'Pop-Up',
+                            Icon(
+                              Icons.check_box,
+                              color: Color.fromARGB(255, 78, 90, 92),
+                            ),
+                          ),
+                          buildListTile(
+                            context,
+                            'In-App',
+                            Icon(
+                              Icons.check_box,
+                              color: Color.fromARGB(255, 78, 90, 92),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                ),
-                buildListTile(
-                  context,
-                  'LED-Farbe',
-                  Icon(
-                    Icons.check_box,
-                    color: Color.fromARGB(255, 78, 90, 92),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10.0),
+                    child: Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(200, 240, 240, 240),
+                        border: Border.all(
+                          color: Color.fromARGB(255, 200, 200, 200),
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(4.0),
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10.0),
+                            child: Text('LED-Farbe'),
+                          ),
+                          buildListTile(
+                            context,
+                            'Rot',
+                            Icon(
+                              Icons.check_box,
+                              color: Color.fromARGB(255, 78, 90, 92),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
@@ -74,13 +200,15 @@ Widget buildHeader(BuildContext context) {
 }
 
 Widget buildListTile(BuildContext context, String text, Widget icon) {
-  return Card(
-    child: ListTile(
-      title: Text(
-        text,
-        style: Theme.of(context).textTheme.bodyText1,
+  return Container(
+    child: Card(
+      child: ListTile(
+        title: Text(
+          text,
+          style: Theme.of(context).textTheme.bodyText1,
+        ),
+        leading: icon,
       ),
-      leading: icon,
     ),
   );
 }
