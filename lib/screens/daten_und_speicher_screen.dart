518995fa7e271be91845200e569ffbbd0979d267
Kepp <jasonstolze@yahoo.de>
2021-06-29 13:29:37 +0200

    Daten und Speicher segmentiert

--- lib/screens/daten_und_speicher_screen.dart
+++ lib/screens/daten_und_speicher_screen.dart
@@ -8,40 +8,82 @@ class DatenUndSpeicherScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: SafeArea(
-        child: Container(
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-              children: [
-                buildHeader(context),
-                buildListTile(
-                  context,
-                  'Daten verwalten',
-                  Icon(
-                    Icons.folder,
-                    color: Color.fromARGB(255, 78, 90, 92),
+        child: SingleChildScrollView(
+          child: Container(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  buildHeader(context),
+                  buildListTile(
+                    context,
+                    'Daten verwalten',
+                    Icon(
+                      Icons.folder,
+                      color: Color.fromARGB(255, 78, 90, 92),
+                    ),
                   ),
-                ),
-                buildListTile(
-                  context,
-                  'Netzwerknutzung',
-                  Icon(
-                    Icons.cloud_circle,
-                    color: Color.fromARGB(255, 78, 90, 92),
+                  buildListTile(
+                    context,
+                    'Netzwerknutzung',
+                    Icon(
+                      Icons.cloud_circle,
+                      color: Color.fromARGB(255, 78, 90, 92),
+                    ),
                   ),
-                ),
-                buildListTile(
-                  context,
-                  'Medien automatisch downloaden',
-                  Icon(
-                    Icons.check_box,
-                    color: Color.fromARGB(255, 78, 90, 92),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10.0),
+                    child: Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(200, 240, 240, 240),
+                        border: Border.all(
+                          color: Color.fromARGB(255, 200, 200, 200),
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(4.0),
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10.0),
+                            child: Text('Medien automatisch downloaden'),
+                          ),
+                          buildListTile(
+                            context,
+                            'Bei mobiler Datenverbindung',
+                            Icon(
+                              Icons.crop_square,
+                              color: Color.fromARGB(255, 78, 90, 92),
+                            ),
+                          ),
+                          buildListTile(
+                            context,
+                            'Bei einer WLAN-Verbindung',
+                            Icon(
+                              Icons.check_box,
+                              color: Color.fromARGB(255, 78, 90, 92),
+                            ),
+                          ),
+                          buildListTile(
+                            context,
+                            'Beim Roaming',
+                            Icon(
+                              Icons.check_box,
+                              color: Color.fromARGB(255, 78, 90, 92),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
+
           ),
-        ),
+        )
       ),
     );
   }
